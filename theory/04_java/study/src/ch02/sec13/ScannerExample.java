27e75e4a451b9d0ecd6f872db275d3535a9c9dbb
Cho2unseo <escces00@gmail.com>
2025-04-14 22:18:22 +0900

    0414

--- theory/04_java/study/src/ch02/sec13/ScannerExample.java
+++ theory/04_java/study/src/ch02/sec13/ScannerExample.java
@@ -1,2 +1,12 @@
-package ch02.sec13;public class ScannerExample {
+package ch02.sec13;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("x 값 입력: ");
+        String strX = sc.nextLine();
+        int x = Integer.parseInt(strX);
+    }
 }
